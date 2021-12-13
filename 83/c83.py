fe5f24cf90f805807a99403a6863c0fb76773fac
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2021-12-13 11:30:00 +0530

    c83

---
+++ 83/c83.py
@@ -0,0 +1,1 @@
+for _ in ' '*int(input()):print(' '.join(map(lambda x:x[1:]+x[0]+'ay',input().split())))
---
+++ 83/temp_solution_file.py
@@ -0,0 +1,2 @@
+def solution():
+    for i in[*open(0)][1:]:print(*[_[1:]+_[0]+"ay"for _ in i.split()])
